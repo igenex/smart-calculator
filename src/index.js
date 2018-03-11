adafaf32d25c8b7afa1b176661983885c3f03f23
Eugene <igenexxx@gmail.com>
2018-03-11 14:43:28 +0300

    All passed

--- src/index.js
+++ src/index.js
@@ -51,15 +51,31 @@ class SmartCalculator {
   }
 
   calculate() {
-    this.cache.reduceRight((prev, curr, i) => {
+
+    let pocket = [];
+    let result = [];
+
+    this.cache.reduceRight((prev, curr, i, array) => {
       if((/(add|subtract)/.test(curr[1]) && /(add|subtract)/.test(prev[1]))
-      ||  !(/(add|subtract)/.test(curr[1])) && /(add|subtract)/.test(prev[1])
-      || (prev[1] === curr[1] && /(add|subtract)/.test(curr[1]))
-      || (curr[1] === 'pow' && !(/(add|subtract)/.test(prev[1])))){
+        ||  !(/(add|subtract)/.test(curr[1])) && /(add|subtract)/.test(prev[1])
+        || (prev[1] === curr[1] && /(add|subtract)/.test(curr[1]))
+        || (curr[1] === 'pow' && !(/(add|subtract)/.test(prev[1])))){
+        if(curr[1] === 'pow' && !(/(add|subtract)/.test(prev[1]))) {
+          pocket.push([prev[0], prev[1]]);
+        }
         return curr;
       } else {
-        this.cache.splice(i,2,[this.getOperation(prev[1], curr[0], prev[0]), curr[1]]);
-        return [this.getOperation(prev[1], curr[0], prev[0]), curr[1]];
+        result = [this.getOperation(prev[1], curr[0], prev[0]), curr[1]];
+        this.cache.splice(i, 2, result);
+
+        if(pocket.length > 0) {
+          result = [this.getOperation(pocket[0][1], array[i][0], pocket[0][0]), curr[1]];
+          this.cache.splice(i,2,result);
+          pocket = [];
+          return result;
+        }
+
+        return result;
       }
     });
 
@@ -89,21 +105,6 @@ class SmartCalculator {
   }
 
 }
-
-const calculator = new SmartCalculator(6);
-
-const value = calculator
-  .pow(2)
-  .pow(1)
-  .add(33)
-  .pow(1)
-  .multiply(2)
-  .multiply(1)
-  .add(39)
-  .multiply(2)
-  .multiply(2); //258
-
-console.log(value);
 
 module.exports = SmartCalculator;
 
